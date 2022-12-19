a3868c88512882a05713185dffa5fd9b6faea043
kanta1207 <kan0712livepool@gmail.com>
2022-12-20 00:03:00 +0900

    created the base of Header component

---
+++ nextjs-projects/with-tailwindcss-app/components/Layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from 'react'
+
+
+type Props = {
+    children : ReactNode;
+    title : string;
+}
+export const Layout = () => {
+  return (
+    <div>Layout</div>
+  )
+}
